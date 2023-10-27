ed0fab32f46b4627ff60700e875620a8e11c9bf7
manhrev <manhnd@ugaming.io>
2023-10-27 09:36:48 +0700

    sample template

---
+++ src/layout/Main.tsx
@@ -0,0 +1,23 @@
+import { styled } from '@mui/material/styles';
+import { DRAWER_WIDTH } from './consts';
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
+  open?: boolean;
+}>(({ theme, open }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3),
+  transition: theme.transitions.create('margin', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  marginLeft: `-${DRAWER_WIDTH}px`,
+  ...(open && {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  }),
+}));
+
+export default Main;
